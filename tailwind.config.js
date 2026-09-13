8e8929ff8be8e8dc859da23a88450b2001eea391
Matheus <qmatheusnogueira@gmail.com>
2026-09-13 20:43:16 -0300

    chore: configura projeto com React Vite e TypeScript

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export default {content:['./index.html','./src/**/*.{js,ts,jsx,tsx}'],theme:{extend:{colors:{brand:{50:'#effdf5',100:'#d9fbe7',400:'#35c97b',500:'#20ad65',600:'#15894f',900:'#103d2a'},ink:'#10251c'}}},plugins:[]}
