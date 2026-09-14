1e1e3fe6633de881d105fa0cc483f8e1703dc893
arthurmaia07 <arthurmaia2007@outlook.com>
2026-09-13 21:29:22 -0300

    adiciona as rotas e o perfil dinamico dos integrantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-function App() {
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Avatar from './pages/Avatar'
+import Contato from './pages/Contato'
+import Faq from './pages/Faq'
+import Gamificacao from './pages/Gamificacao'
+import Home from './pages/Home'
+import IntegranteDetalhe from './pages/IntegranteDetalhe'
+import Integrantes from './pages/Integrantes'
+import NotFound from './pages/NotFound'
+import Sobre from './pages/Sobre'
+
+export default function App() {
   return (
-    <div>
-      <h1>BraTech</h1>
-      <p>Projeto React funcionando!</p>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/gamificacao" element={<Gamificacao />} />
+        <Route path="/avatar" element={<Avatar />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/integrantes" element={<Integrantes />} />
+        <Route path="/integrantes/:id" element={<IntegranteDetalhe />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
-
-export default App
